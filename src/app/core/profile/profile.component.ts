bee8bd7d833a52f8e90d00bdc0454729f4eda4e0
LuisDonaldo2003 <l21930214@cdaltamirano.tecnm.mx>
2025-03-22 18:04:28 -0600

    Updated

--- src/app/core/profile/profile.component.ts
+++ src/app/core/profile/profile.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/shared/auth/auth.service';
+import { Component } from '@angular/core';
 import { routes } from 'src/app/shared/routes/routes';
 
 @Component({
@@ -7,24 +6,6 @@ import { routes } from 'src/app/shared/routes/routes';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
   public routes = routes;
-  public user: any;
-
-  constructor(private authService: AuthService) {}
-
-  ngOnInit(): void {
-    this.authService.user$.subscribe(user => {
-      console.log('User data:', user); // Verificar los datos del usuario
-      this.user = user;
-    });
-  }
-
-  getRole(){
-    let RoleName = "";
-    this.user.roles.forEach((rol:any) => {
-      RoleName = rol;
-    });
-    return RoleName;
-  }
 }
